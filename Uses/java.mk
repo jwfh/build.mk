663ea2ec92a4ea1cffad762b6680a4ab1bf77b84
Jacob William House <jacob@jacobhouse.ca>
2022-02-19 00:26:16 -0330

    uses: Add empty files to Uses/ for lang support
    I would like to support CMake, Docker, Java, Python, and Terraform
    projects being built with this build system extension.

---
+++ Uses/java.mk
@@ -0,0 +1,5 @@
+.if !defined(_INCLUDE_USES_JAVA_MK)
+_INCLUDE_USES_JAVA_MK=	yes
+
+
+.endif #!defined(_INCLUDE_USES_JAVA_MK)
---
+++ Uses/terraform.mk
@@ -0,0 +1,5 @@
+.if !defined(_INCLUDE_USES_TERRAFORM_MK)
+_INCLUDE_USES_TERRAFORM_MK=	yes
+
+
+.endif #!defined(_INCLUDE_USES_TERRAFORM_MK)
