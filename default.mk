d58626684b061c7447b3d92a83bc2c8e37f09d4c
Jacob House <jacob@jacobhouse.ca>
2022-02-25 20:28:57 -0330

    :seedling: Create 'targets' target

--- default.mk
+++ default.mk
@@ -1,6 +1,9 @@
 # Included on all `make(1)' executions.
 # Defines default variables and targets.
 
+.if !defined(_INCLUDE_DEFAULT_MK)
+_INCLUDE_DEFAULT_MK=	yes
+
 CURL_COMMAND=		curl -sSfm1
 EC2_METADATA_URI=	http://169.254.169.254/latest/meta-data
 
@@ -14,3 +17,15 @@ make${f}check:
 .endif
 .endif
 .endfor
+
+
+targets:
+	@printf '${.ALLTARGETS:@.t.@${.t.}${.newline}@}'
+
+
+.SHELL: name=bash path=/bin/bash hasErrCtl=true \
+	check="set -e" ignore="set +e" \
+	echo="set -v" quiet="set +v" filter="set +v" \
+	echoFlag=v errFlag=e newline="'\n'"
+
+.endif #!defined(_INCLUDE_DEFAULT_MK)
